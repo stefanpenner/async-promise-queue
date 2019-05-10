dea1913c4515293ff89990c79773b18008229d48
Stefan Penner <stefan.penner@gmail.com>
2019-05-10 06:50:01 -0600

    skip the the async module entirely if we have no work

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ var debug = require('debug')('async-promise-queue:');
 module.exports = function queue(worker, work, concurrency) {
   debug('started, with concurrency=' + concurrency)
   return new Promise(function(resolve, reject) {
-    var q = require('async').queue(worker, concurrency);
-    var firstError;
-    q.drain = resolve;
-    q.error = function(error) {
-      if (firstError === undefined) {
-        // only reject with the first error;
-        firstError = error;
-      }
+    if (work.length === 0) {
+      resolve();
+    } else {
+      var q = require('async').queue(worker, concurrency);
+      var firstError;
+      q.drain = resolve;
+      q.error = function(error) {
+        if (firstError === undefined) {
+          // only reject with the first error;
+          firstError = error;
+        }
 
-      // don't start any new work
-      q.kill();
+        // don't start any new work
+        q.kill();
 
-      // but wait until all pending work completes before reporting
-      q.drain = function() {
-        reject(firstError);
+        // but wait until all pending work completes before reporting
+        q.drain = function() {
+          reject(firstError);
+        };
+
       };
-
-    };
-    q.push(work);
+      q.push(work);
+    }
   });
 };
 
